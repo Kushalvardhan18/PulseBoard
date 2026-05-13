76c4d1d0e01eab68d1c756b3fdad9b2415565aab
Kushal Vardhan <vardhank140102@gmail.com>
2026-05-13 11:10:19 +0530

    db connected

---
+++ backEnd/src/utils/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+import dotenv from "dotenv"
+dotenv.config()
+
+//export a db function to connect to database
+const db = () => {
+    mongoose.connect(process.env.MONGO_URL).then(() => {
+        console.log("Connected to mongoDB database");
+
+    })
+        .catch((err) => {
+            console.error("Error:", err);
+
+        })
+}
+export default db
