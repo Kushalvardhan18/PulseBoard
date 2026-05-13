8f5b5234cdd1bd0ea70f9f24d7fa7e61d1433e46
Kushal Vardhan <vardhank140102@gmail.com>
2026-05-13 11:10:48 +0530

    db connected

--- backEnd/src/index.js
+++ backEnd/src/index.js
@@ -1,5 +1,18 @@
 import express from "express"
+import dotenv from "dotenv"
+import db from "./utils/db.js"
 
+dotenv.config()
 const app= express()
+
 app.use(express.json())
 
+db()
+
+// app.use("/api/v1/users",userRoutes)
+// app.use("/api/v1/polls",pollRoutes)
+// app.use("/api/v1/users",userRoutes)
+const port = process.env.PORT
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
